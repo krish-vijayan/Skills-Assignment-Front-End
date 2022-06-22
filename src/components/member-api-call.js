e7ccd386d0c6832b84ee601724583c16d8b7f228
krish-vijayan <96496079+krish-vijayan@users.noreply.github.com>
2022-06-22 17:04:31 -0400

    Update member-api-call.js

--- src/components/member-api-call.js
+++ src/components/member-api-call.js
@@ -4,7 +4,7 @@ function MemberApi(id){ //pass in your id (index number in group-data.js)
     const [ members, setMembers ] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:8080/')
+        fetch('https://skills-assignment-back-end.herokuapp.com/')
         .then((response) => response.json())
         .then((json) => {
             setMembers(json.AllMembers[id])
