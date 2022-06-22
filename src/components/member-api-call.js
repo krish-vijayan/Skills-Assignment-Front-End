248e1d7ac7e84ac6c33989c209db822380694566
krish-vijayan <krishvijayan55@gmail.com>
2022-06-22 16:06:38 -0400

    Third commit

--- src/components/member-api-call.js
+++ src/components/member-api-call.js
@@ -4,7 +4,7 @@ function MemberApi(id){ //pass in your id (index number in group-data.js)
     const [ members, setMembers ] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:8080/members')
+        fetch('http://localhost:8080/')
         .then((response) => response.json())
         .then((json) => {
             setMembers(json.AllMembers[id])
